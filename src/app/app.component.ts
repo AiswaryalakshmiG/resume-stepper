fa2e7704b6b0dc77eb74dc727edd328fb1d1902b
AiswaryalakshmiG <aiswaryalakshmi1502@gmail.com>
2025-04-30 16:06:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(pdfMake as any).vfs = pdfFonts;
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'resume-builder';
+export class AppComponent  {
+  showData={}
+      constructor(private fb :FormBuilder){}
+  personalInformation=this.fb.group({
+    fullname:['', Validators.required],
+    email:['', Validators.required],
+    Phone:['', Validators.required],
+    linkedin:['', Validators.required],
+    address:[''],
+  });
+
+  objective=this.fb.group({
+    objective:['', Validators.required]
+  });
+
+  education=this.fb.group({
+    degree:['' ,Validators.required],
+    institution:['', Validators.required],
+    year:['', Validators.required],
+    percentage:['', Validators.required]
+  });
+
+  experiences=this.fb.group({
+    role:['', Validators.required],
+    companyName:['', Validators.required],
+    duration:['', Validators.required]
+  });
+
+  project=this.fb.group({
+    title:['', Validators.required],
+    description:['', Validators.required],
+    tech:['', Validators.required],
+  });
+  skills=this.fb.group({
+
+  });
+
+  submit(){
+    if(
+    this.personalInformation.valid&&
+    this.objective.valid&&
+    this.education.valid&&
+    this.experiences.valid&&
+    this.project.valid&&
+    this.skills.valid)
+    this.showData={
+      pernalinfo:this.personalInformation.value,
+      objective:this.objective.value,
+      edu:this.education.value,
+      experience:this.education.value,
+      proj:this.project.value,
+      skill:this.skills.value
+    }
+    console.log(this.showData);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
